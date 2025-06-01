5beace594488bc9fd5f30ba3d983b30604f72760
toftpokk <toftpokk@protonmail.com>
2025-06-01 17:22:07 +0700

    fix script injection

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,75 @@
-use std::{collections::HashMap, env};
+use std::env;
 
 use actix_web::{
     App, HttpResponse, HttpServer, Responder,
     cookie::time::{OffsetDateTime, format_description::well_known::Rfc3339},
     get, post, web,
 };
-use diesel::{QueryDsl, SqliteConnection};
+use askama::Template;
 use models::{DefinitionRepository, WordRepository};
-use schema::definitions;
-use serde::{Deserialize, Serialize};
 
 mod database;
 mod models;
 mod schema;
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct SearchForm {
     word: String,
 }
 
+#[derive(Template)]
+#[template(
+    ext = "html",
+    source = r##"
+        <span class="word-tag" hx-post="/search" hx-vals='{"word": "{{ word }}"}' hx-target="#results">{{ word }}</span>
+    "##
+)]
+struct WordTagTemplate<'a> {
+    word: &'a String,
+}
+
 // Helper function to generate HTML for word tags
 fn generate_word_tags(words: &[String], tag_type: &str) -> String {
     words
         .iter()
-        .map(|word| {
-            format!(
-                r##"<span class="word-tag" hx-post="/search" hx-vals='{{"word": "{}"}}' hx-target="#results">{}</span>"##,
-                word, word
-            )
-        })
+        .map(|word: &String| WordTagTemplate { word: word }.render().unwrap())
         .collect::<Vec<_>>()
         .join("")
 }
 
+#[derive(Template)]
+#[template(
+    ext = "html",
+    source = r#"
+    <div class="word-result">
+        <div class="word-header">
+            <div class="word-title">{{ title }}</div>
+        </div>
+        <div class="definitions">
+        {% for definition in definitions %}
+            <div class="definition-group">
+                <div class="definition-text">{{ definition.definition_header }}</div>
+            </div>
+            {{ definition.definition }}
+        {% endfor %}
+        </div>
+    </div>
+    "#
+)]
+struct WordResultTemplate<'a> {
+    title: &'a String,
+    definitions: Vec<models::Definition>,
+}
+
 // Generate HTML for word result
 fn generate_word_result_html(word: &models::Word, definitions: Vec<models::Definition>) -> String {
-    let definitions_html = definitions
-        .iter()
-        .map(|def| {
-            format!(
-                r#"<div class="definition-group">
-                    <div class="definition-text">{}</div>
-                    {}
-                </div>"#,
-                def.definition_header, def.definition,
-            )
-        })
-        .collect::<Vec<_>>()
-        .join("");
-
-    format!(
-        r#"<div class="word-result">
-            <div class="word-header">
-                <div class="word-title">{}</div>
-            </div>
-
-            <div class="definitions">
-                {}
-            </div>
-        </div>"#,
-        word.word, definitions_html,
-    )
+    return WordResultTemplate {
+        title: &word.word,
+        definitions: definitions,
+    }
+    .render()
+    .unwrap();
 }
 
 struct AppState {
@@ -95,6 +104,20 @@ async fn quick_links() -> impl Responder {
         .body(quick_links);
 }
 
+#[derive(Template)]
+#[template(
+    ext = "html",
+    source = r#"
+    <div class="error">
+        <h3>Word not found</h3>
+        <p>Sorry, we couldn't find "{{ search_word }}" in our dictionary. Try checking the spelling or searching for a different word.</p>
+    </div>
+    "#
+)]
+struct ErrorTemplate<'a> {
+    search_word: &'a String,
+}
+
 #[post("/search")]
 async fn search_word(form: web::Form<SearchForm>) -> impl Responder {
     let mut connection = database::establish_connection();
@@ -115,15 +138,11 @@ async fn search_word(form: web::Form<SearchForm>) -> impl Responder {
             .body(welcome_html);
     }
 
-    // let dictionary = get_mock_dictionary();
-
-    let error_html = format!(
-        r#"<div class="error">
-            <h3>Word not found</h3>
-            <p>Sorry, we couldn't find "{}" in our dictionary. Try checking the spelling or searching for a different word.</p>
-        </div>"#,
-        search_word
-    );
+    let error_html = ErrorTemplate {
+        search_word: &search_word,
+    }
+    .render()
+    .unwrap();
 
     let word = match WordRepository::find_by_word(&mut connection, search_word) {
         Ok(word) => word,
